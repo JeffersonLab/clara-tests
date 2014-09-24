c82546820527879358e756d3fbe2a64cb85fa551
smancill <smancill.m@gmail.com>
2014-09-24 11:50:05 -0300

    Add initial puppet provision manifest
    - Fix manifest_file variable.
    - Don't set module_path for now.

---
+++ acceptance/vagrant/provision/manifests/default.pp
@@ -0,0 +1,27 @@
+class devtools {
+    Package {
+        ensure => installed,
+    }
+
+    package { [
+        "openjdk-7-jdk",
+        "ant",
+    ]:
+    }
+}
+
+# Fix "localhost" IP in Java
+class network {
+    $private_ip = $hostname ? {
+        'platform' => '10.11.1.100',
+        'dpe1'     => '10.11.1.101',
+        'dpe2'     => '10.11.1.102',
+    }
+
+    host {
+        $hostname: ip => $private_ip
+    }
+}
+
+include devtools
+include network
