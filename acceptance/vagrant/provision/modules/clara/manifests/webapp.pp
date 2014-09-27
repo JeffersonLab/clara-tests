687861423f888cfab1214f751c394b2157a9643e
smancill <smancill.m@gmail.com>
2014-09-26 23:38:34 -0300

    Install the Ruby version required by Clara Web App
    - Download and install RVM.
    
    - Use RVM to download and install the proper Ruby version to
      run the Clara Web App.
    
    - Don't use the system package for Bundler, use the version downloaded
      with Ruby.
    
    - Now the webapp provision, using the RVM Ruby, can successfully install
      all gems (the libpq-dev is required first to install the pg gem).
    
    - Install Node.js as the Javascript runtime.

--- acceptance/vagrant/provision/modules/clara/manifests/webapp.pp
+++ acceptance/vagrant/provision/modules/clara/manifests/webapp.pp
@@ -4,7 +4,23 @@ class clara::webapp {
     }
 
     package { [
-        "bundler",
+        "curl",
+        "libpq-dev",
+        "nodejs",
     ]:
+    }
+
+    exec { 'install-rvm':
+        command => "/bin/su -l --command='curl -sSL https://get.rvm.io | bash -s stable --ignore-dotfiles' vagrant",
+        creates => "/home/vagrant/.rvm/bin/rvm",
+        require => Package['curl'],
+        logoutput => true,
+    }
+
+    exec {'install-ruby':
+        command =>"/bin/su -l --command='rvm install 2.1.2' vagrant",
+        require => [ Exec['install-rvm'], File['/home/vagrant/.profile'] ],
+        timeout => 1800,
+        logoutput => true,
     }
 }
