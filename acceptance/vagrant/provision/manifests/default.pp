c5cf334b4dd10f85e8fe49f14df77e9ac232bd90
smancill <smancill.m@gmail.com>
2014-10-01 00:19:11 -0300

    Small puppet class rename on default manifest
    Rename the 'network' class to 'common', since it will be used for a few
    more common settings.

--- acceptance/vagrant/provision/manifests/default.pp
+++ acceptance/vagrant/provision/manifests/default.pp
@@ -1,17 +1,17 @@
-# Fix "localhost" IP in Java
-class network {
+class common {
     $private_ip = $hostname ? {
         'platform' => '10.11.1.100',
         'dpe1'     => '10.11.1.101',
         'dpe2'     => '10.11.1.102',
     }
 
+    # Fix "localhost" IP in Java
     host {
         $hostname: ip => $private_ip
     }
 }
 
-include network
+include common
 include locales
 include clara
 include tools
