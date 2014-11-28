276117da72e6804287b11281d252f48736779e47
smancill <smancill.m@gmail.com>
2014-11-28 16:22:02 -0300

    Fix RVM download
    Install the required GPG key first.

--- acceptance/provision/modules/clara/manifests/webapp.pp
+++ acceptance/provision/modules/clara/manifests/webapp.pp
@@ -5,15 +5,23 @@ class clara::webapp {
 
     package { [
         "curl",
+        "gnupg",
         "libpq-dev",
         "nodejs",
     ]:
     }
 
+    exec { 'rvm-key':
+        command   => "/bin/su -l --command='gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3' vagrant",
+        require   => Package['gnupg'],
+        unless    => "/bin/su -l --command 'gpg --list-keys | grep -q D39DC0E3' vagrant",
+        logoutput => true,
+    }
+
     exec { 'install-rvm':
         command => "/bin/su -l --command='curl -sSL https://get.rvm.io | bash -s stable --ignore-dotfiles' vagrant",
         creates => "/home/vagrant/.rvm/bin/rvm",
-        require => Package['curl'],
+        require => [ Exec['rvm-key'], Package['curl'] ],
         logoutput => true,
     }
 
