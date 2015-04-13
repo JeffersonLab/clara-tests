95bf0bf2822fe35b73498176c9dec1302d6756d0
Sebastián Mancilla <smancill@jlab.org>
2015-04-13 11:49:20 -0300

    No need for Ruby and Rails anymore
    The new clara-web is using Python.

--- acceptance/provision/modules/clara/manifests/webapp.pp
+++ acceptance/provision/modules/clara/manifests/webapp.pp
@@ -2,34 +2,4 @@ class clara::webapp {
     Package {
         ensure => latest,
     }
-
-    package { [
-        "curl",
-        "gnupg",
-        "libpq-dev",
-        "libsqlite3-dev",
-        "nodejs",
-    ]:
-    }
-
-    exec { 'rvm-key':
-        command   => "/bin/su -l --command='gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3' vagrant",
-        require   => Package['gnupg'],
-        unless    => "/bin/su -l --command 'gpg --list-keys | grep -q D39DC0E3' vagrant",
-        logoutput => true,
-    }
-
-    exec { 'install-rvm':
-        command => "/bin/su -l --command='curl -sSL https://get.rvm.io | bash -s stable --ignore-dotfiles' vagrant",
-        creates => "/home/vagrant/.rvm/bin/rvm",
-        require => [ Exec['rvm-key'], Package['curl'] ],
-        logoutput => true,
-    }
-
-    exec {'install-ruby':
-        command =>"/bin/su -l --command='rvm install 2.1.2' vagrant",
-        require => [ Exec['install-rvm'], File['/home/vagrant/.profile'] ],
-        timeout => 1800,
-        logoutput => true,
-    }
 }
